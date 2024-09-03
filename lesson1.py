e28ba1d7e396749fe58ebfa33bd6c0ce2d007245
Lemus <aglemus@cps.k12.il.us>
2024-09-03 12:39:42 -0500

    made a comment

--- lesson1.py
+++ lesson1.py
@@ -28,3 +28,6 @@ print("Your favorite food is", food)
 favorite_animal = input("What is your favorite animal?")
 print("Your favorite animal is a"+ favorite_animal)
 # the plus sign is used to concatenate strings
+
+
+# lets do some more practice
